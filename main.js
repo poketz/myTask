b7f446d341a48e2649102c7e058edc47b014002a
hiroshi <tkmrh49@gmail.com>
2020-07-02 11:20:44 +0900

    input-display-app 完成

--- main.js
+++ main.js
@@ -1,7 +1,14 @@
-const input_field =  //id='input-field'の要素
-const display =   //id='display'の要素
-const button =   //id='button'の要素
+const input_field = document.getElementById('input-field') //id='input-field'の要素
+const display = document.getElementById('display')  //id='display'の要素
+const button = document.getElementById('button')  //id='button'の要素
 
-function inputFieldToDisplay(){
+button.addEventListener('click', inputFieldToDisplay, false);
+
+function inputFieldToDisplay() {
+    
     //input_fieldの内容をdisplayに反映させる関数
+    display.innerHTML = input_field.value
+    
 }
+
+
