fa285cdb668ff07801cd999e21984c52ec090959
hiroshi <tkmrh49@gmail.com>
2020-07-02 10:07:01 +0900

    Create input-display-app

---
+++ main.js
@@ -0,0 +1,7 @@
+const input_field =  //id='input-field'の要素
+const display =   //id='display'の要素
+const button =   //id='button'の要素
+
+function inputFieldToDisplay(){
+    //input_fieldの内容をdisplayに反映させる関数
+}
