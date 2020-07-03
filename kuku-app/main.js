8add8ade51dbf9b53f22cfe4a4cfcb7a016fb05f
hiroshi <tkmrh49@gmail.com>
2020-07-04 05:09:09 +0900

    わからん

--- kuku-app/main.js
+++ kuku-app/main.js
@@ -1,5 +1,13 @@
 //このファイルを編集して九九の表を作成してください
 
-
+    for (let i = 1; i < 10; i++) {
+        console.log(i);
+    }
+    for (let j = 1; j < 10; j++) {
+        console.log(j);
+    }
+    let table = document.getElementById('targetTable');
+    let newRow = table.insertRow();
+    newRow.insertCell();
 
 
