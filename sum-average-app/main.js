c8e7a6e872ed7d4d5182556fe80ef84bc2bc83b9
hiroshi <tkmrh49@gmail.com>
2020-07-04 22:06:37 +0900

    Create sum-average-app

---
+++ sum-average-app/main.js
@@ -0,0 +1,12 @@
+//この部分は編集しないでください↓
+//
+const list = document.getElementById("list");
+const sum = document.getElementById("sum");
+const ave = document.getElementById("ave");
+//
+
+//ここに100,90,80,70の点数の配列を作成し，格納したデータを「点数:100,90,80,70」となるように表示してください
+
+//for文を用いて点数の和を計算し,結果を「合計:{和の数}」となるように表示してください
+
+//平均の値も計算して,結果を「平均:{平均の数}」となるように表示してください
