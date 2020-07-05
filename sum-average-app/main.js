e61c31a26311fb7ecb843ac13a5460eb504db71b
hiroshi <tkmrh49@gmail.com>
2020-07-05 15:24:15 +0900

    それぞれの点数と合計を出力

--- sum-average-app/main.js
+++ sum-average-app/main.js
@@ -6,7 +6,12 @@ const ave = document.getElementById("ave");
 //
 
 //ここに100,90,80,70の点数の配列を作成し，格納したデータを「点数:100,90,80,70」となるように表示してください
-
+let score = [100,90,80,70];
+list.innerHTML = `点数:${score}`;
 //for文を用いて点数の和を計算し,結果を「合計:{和の数}」となるように表示してください
-
+let sumScore = 0;
+for(let i = 0; i < score.length; i++) {
+    sumScore += score[i];
+}
+sum.innerHTML = `合計:${sumScore}`
 //平均の値も計算して,結果を「平均:{平均の数}」となるように表示してください
