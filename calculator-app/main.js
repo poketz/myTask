c5cbb84eb06c41554899bd5c9a5beeecc1ea9bec
hiroshi <tkmrh49@gmail.com>
2020-07-10 01:41:16 +0900

    feat: htmlの表示

--- calculator-app/main.js
+++ calculator-app/main.js
@@ -1,18 +1,10 @@
-//初めに使用する変数を宣言しましょう
 
-
-function sign(btn) {
-    //<div id = "id_buttonContainer">に符号を表示させる関数を記述しましょう
-
-}
-
-function run() {
-    //符号に応じた計算を行い，<div id="id_answer">にその結果を表示する関数を記述しましょう
-
-}
-
-function reset() {
-    // 初期状態にリセットする関数を記述しましょう
-    // 変数をリセットするとともに，空欄の中身や結果の表示も初期状態に戻しましょう
-
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+let btn3 = document.getElementById("btn3");
+let btn4 = document.getElementById("btn4");
+let test = function () {
+    let input1 = document.getElementById("input1").value;
+    let input2 = document.getElementById("input2").value;
+    console.log(input1,input2)
 }
